ae81304943a0a5570b37b81eb432c4b126dd7693
Solina <117688013+L5462@users.noreply.github.com>
2023-12-19 01:41:08 -0500

    Update Lottery.java

--- Lottery.java
+++ Lottery.java
@@ -24,7 +24,69 @@ public class Lottery{
   } // end of main method
 
   public static int numRandom(int min, int max) {
-        return (int)(Math.random() * (max - min + 1) + min);
+    return (int)(Math.random() * (max - min + 1) + min);
+  } // end of numRandom method
+  
+
+  public static boolean isValid(int[] arr, int num) {
+    if(num < 1 || num > 69) {
+      return false;
     }
+    
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[i] == num) {
+        return false;
+      }
+    } // end for loop
+    
+    return true;
+  } // end of isValid method
+  
+
+  public static String getPrize(int wr, int rr){
+    String result = "";
+    
+    // when there's no redball
+    if (rr == 0) {
+      if (wr == 5){
+        result += "You win $1 Million!";
+      }
+      else if (wr == 4){
+        result += "You win $100!";
+      }
+      else if (wr == 3){
+        result += "You win $7!";
+      }
+    
+    // when the user didn't get anything
+      else{
+        result += "You didn't win anything :(";
+      }
+    } // end if statement
+      
+    // when there's redball
+    if (rr == 1){
+      if (wr == 5){
+        result += "You win the Grand Prize!";
+      }
+      else if (wr == 4){
+        result += "You win $50,000!";
+      }
+      else if (wr == 3){
+        result += "You win $100!";
+      }
+      else if (wr == 2){
+        result += "You win $7!";
+      }
+      else if (wr == 1){
+        result += "You win $4!";
+      }
+      else if (wr == 0){
+        result += "You win $4!";
+      }
+    } // end else if statement
+
+    retrun result;
+  } // end of getPrize method
   
 } // end of class
